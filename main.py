1073f619a7eeb6073e2e633481e08aee2d6d1dfb
JohnnyBannanis <maldojota@gmail.com>
2020-08-28 02:18:24 -0400

    color scale fixed

--- main.py
+++ main.py
@@ -1,21 +1,21 @@
-from pynput import keyboard
-from plot import *
 import matplotlib.pyplot as plt
 import matplotlib as mpl
+from pynput import keyboard
+from plot import *
 from matplotlib import cm
-
-import numpy as np
 
 keys = {}
 
 def on_press(key):
     try:
-        if(key.char in keys):
-            keys[key.char] += 1
+        aux = key.char.lower()
+        if(aux in keys):
+            keys[aux] += 1
         else:
-            keys[key.char] = 1
+            keys[aux] = 1
     except AttributeError:
         aux = str(key).replace('Key.','')
+        print(aux)
         if(aux in keys):
             keys[aux] += 1
         else:
@@ -25,8 +25,8 @@ def on_release(key):
     if key == keyboard.Key.esc:
         # Stop listener
         print(keys)
+        
         return False
-
 
 if __name__ == "__main__":
 
@@ -40,20 +40,25 @@ if __name__ == "__main__":
         on_release=on_release)
     listener.start()
 
-
     x = []
     y = []
+    v = []
 
     for i in key_pos:
-        x.append(key_pos[i][0])
-        y.append(key_pos[i][1])
-        
-    colors = np.random.rand(3)
-    area = (30 * np.random.rand(1))**2  # 0 to 15 point radii
+        if i in keys:
+            x.append(key_pos[i][0])
+            y.append(key_pos[i][1])
+            v.append(keys.get(i))
+            
     
+    area = (50)**2  # 0 to 15 point radii
+
     img = plt.imread("la_keyboard.png")
     fig, ax = plt.subplots()
     ax.imshow(img)
 
-    plt.scatter(x, y, s=area, c=cm.inferno(np.abs(y)), alpha=1)
+    ##plt.scatter(x, y, s= area, c= cm.plasma(vf), alpha=0.6 )
+    plt.scatter(x, y, s= area, c= v, cmap='plasma', vmax=sum(v) ,alpha=0.5)
+    plt.colorbar()
     plt.show()
+    #
